5de937617840d23e7b9b22b646853e289d75eb68
CentOS User <centos_user@localhost.localdomain>
2014-05-29 16:04:37 -0300

    Added the first version of the PRINT problem solution.

---
+++ print.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> returnPrimes(int limitNumber)
+{
+    vector<int> primeList;
+    primeList.push_back(2);
+    
+    for(int primeCandidate = 3; primeCandidate <= limitNumber; primeCandidate+=2)
+    {
+       bool isPrime = true;
+       for(vector<int>::iterator primeFactor = primeList.begin(); primeFactor != primeList.end(); primeFactor++)
+       {
+           if(primeCandidate % (*primeFactor) == 0)
+           {
+               isPrime = false;
+               break;
+           }
+       }
+       if(isPrime)
+       {
+           primeList.push_back(primeCandidate);
+       }
+    }
+    return primeList;
+}
+
+int main()
+{
+    int amountIntervals;
+    int bottomLimit, upperLimit;
+    scanf("%d", &amountIntervals);
+    
+    for(int index=0; index < amountIntervals; index++)
+    {
+        scanf("%d %d", &bottomLimit, &upperLimit);
+        vector<int> primeList = returnPrimes(upperLimit);
+        for(vector<int>::iterator prime = primeList.begin(); prime != primeList.end(); prime++)
+        {
+            if(*prime >= bottomLimit)
+            {
+                printf("%d\n", *prime);
+            }
+        }
+    }
+    return 0;
+}
