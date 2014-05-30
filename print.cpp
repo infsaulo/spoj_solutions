9d0639b2d8e04e938880c04584789939952327b6
CentOS User <centos_user@localhost.localdomain>
2014-05-30 10:28:47 -0300

    Implemented erathostenes sieve to PRINT problem

--- print.cpp
+++ print.cpp
@@ -7,36 +7,148 @@ using namespace std;
 
 deque<int> returnPrimes(int limitNumber)
 {
-    deque<int> exceptedPrimeList(1, 2);
-    deque<int> testPrimeList(1, 3);
-    deque<int> skipSet(1, 2);
+    int sqrtLimit = (int)sqrt((float)limitNumber);
+    int primeCandidate = 2;
+    deque<int> skipSet(1, primeCandidate);
+    deque<int> exceptedPrimeList(1, primeCandidate);
+    primeCandidate++;
+    deque<int> rangeSkipSet(1, skipSet[0]);
+    deque<int> testPrimeList(1, primeCandidate);
+
+    int indexCurrentSkipSetRange = 0;
+    rangeSkipSet.push_back(rangeSkipSet[indexCurrentSkipSetRange] * testPrimeList[0]);
+    
+    deque<int> extendedPrimeList;
+    primeCandidate+=skipSet[0];
+    int nextPrimeCandidate = primeCandidate;
+    testPrimeList.push_back(nextPrimeCandidate);
      
-    for(int primeCandidate = 3; primeCandidate <= limitNumber; primeCandidate+=skipSet[0])
+    while(nextPrimeCandidate < limitNumber)
     {
-       bool isPrime = true;
-       int primeCandidateSqrt = (int)sqrt((float)primeCandidate);
-       for(int index=0; index < testPrimeList.size() && testPrimeList[index] <= primeCandidateSqrt; index++)
-       {
-           if(primeCandidate % testPrimeList[index] == 0)
-           {
-               isPrime = false;
-               break;
-           }
-       }
+        indexCurrentSkipSetRange++;
+        while(nextPrimeCandidate < rangeSkipSet[indexCurrentSkipSetRange] + 1)
+        {
+            for(deque<int>::iterator n = skipSet.begin(); n != skipSet.end(); n++)
+            {
+                nextPrimeCandidate = primeCandidate + *n;
+                
+                if(nextPrimeCandidate > limitNumber)
+                {
+                    break;
+                }
+                
+                if(nextPrimeCandidate <= rangeSkipSet[indexCurrentSkipSetRange] + 1)
+                {
+                    primeCandidate = nextPrimeCandidate;
+                }
+                
+                int sqrtNextPrimeCandidate = (int)sqrt((float)nextPrimeCandidate);
+                bool isPrime = true;
+                for(int index=0; index < (int)testPrimeList.size() && sqrtNextPrimeCandidate < testPrimeList[index]; index++)
+                {
+                    if(nextPrimeCandidate % testPrimeList[index] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+               
+                if(isPrime)
+                {
+                    if(nextPrimeCandidate <= sqrtLimit)
+                    {
+                        testPrimeList.push_back(nextPrimeCandidate);
+                    }
+                    else
+                    {
+                        extendedPrimeList.push_back(nextPrimeCandidate);
+                    }
+                }
+            }
+            
+            if(nextPrimeCandidate > limitNumber)
+            {
+                break;
+            }
 
-       if(isPrime)
-       {
-           testPrimeList.push_back(primeCandidate);
-       }
-    }
+            int lastRelevantPrimeCandidate = primeCandidate;
+            deque<int> nextSkipSet;
+            while(primeCandidate < (rangeSkipSet[indexCurrentSkipSetRange]+1)*2 - 1)
+            {
+                for(deque<int>::iterator n=skipSet.begin(); n != skipSet.end(); n++)
+                {
+                    nextPrimeCandidate = primeCandidate + *n;
+                    if(nextPrimeCandidate > limitNumber)
+                    {
+                        break;
+                    }
+                    
+                    int sqrtNextPrimeCandidate = (int)sqrt((float)nextPrimeCandidate);
+                    bool isPrime = true;
+                    for(int index=0; index < (int)testPrimeList.size() && sqrtNextPrimeCandidate < testPrimeList[index]; index++)
+                    {
+                        if(nextPrimeCandidate % testPrimeList[index] == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                   
+                    if(isPrime)
+                    {
+                        if(nextPrimeCandidate <= sqrtLimit)
+                        {
+                            testPrimeList.push_back(nextPrimeCandidate);
+                        }
+                        else
+                        {
+                            extendedPrimeList.push_back(nextPrimeCandidate);
+                        }
+                    }
+ 
+                    if(nextPrimeCandidate % testPrimeList[0] != 0)
+                    {
+                        nextSkipSet.push_back(nextPrimeCandidate - lastRelevantPrimeCandidate);
+                        lastRelevantPrimeCandidate = nextPrimeCandidate;
+                    }
+                    
+                    primeCandidate = nextPrimeCandidate;
+                    
+                }
 
+                if(nextPrimeCandidate > limitNumber)
+                {
+                    break;
+                }
+            }
+
+            if(nextPrimeCandidate > limitNumber)
+            {
+                break;
+            }
+
+            skipSet = nextSkipSet;
+            exceptedPrimeList.push_back(testPrimeList[0]);
+            testPrimeList.pop_front();
+            rangeSkipSet.push_back(rangeSkipSet[indexCurrentSkipSetRange] * testPrimeList[0]);
+            nextPrimeCandidate = lastRelevantPrimeCandidate;
+        }
+
+    }   
+ 
     while(!exceptedPrimeList.empty())
     {
         testPrimeList.push_front(exceptedPrimeList.back());
         exceptedPrimeList.pop_back();
     }
 
-    return testPrimeList;
+    while(!testPrimeList.empty())
+    {
+        extendedPrimeList.push_front(testPrimeList.back());
+        testPrimeList.pop_back();
+    }
+
+    return extendedPrimeList;
 }
 
 int main()
