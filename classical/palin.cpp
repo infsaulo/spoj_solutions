f5a2700023881da53babbcdafeb37b1db2a1cce7
Saulo Ricci <infsaulok@gmail.com>
2014-06-20 22:04:34 -0300

    Adding a new version of the solution of PALIN classical problem. Now the algorithm doesn’t consider increment number by number and decides if the current number is palindrome. Instead it is work O(K) where K is the amount of digits of the input number.

--- classical/palin.cpp
+++ classical/palin.cpp
@@ -1,66 +1,106 @@
 #include <deque>
 #include <algorithm>
 #include <cstdio>
-#include <iostream>
 
-#define MAX_AMOUNT_DIGITES 1000000
+#define MAX_AMOUNT_DIGITES 1000001
 using namespace std;
 
-bool isPalindrome(deque<char> palindrome)
+bool isLessThan(deque<char>& palindrome, char* originalNumber, int originalSize)
 {
-    deque<char>::iterator it1 = palindrome.begin();
-    deque<char>::iterator it2 = palindrome.end();
-    it2--;
-
-    while(it2 > it1)
+    if(originalSize < palindrome.size())
     {
-        if(*it2 != *it1)
+        return false;
+    }
+
+    for(int index = 0; index < originalSize; index++)
+    {
+        if(palindrome[index] > originalNumber[index])
         {
             return false;
         }
-        it2--;
-        it1++;
+        if(palindrome[index] < originalNumber[index])
+        {
+            return true;
+        }
     }
 
     return true;
 }
 
-void addOne(deque<char>& palindrome)
+void makeHalfPalindrome(deque<char>& palindrome, int indexPivot1, int indexPivot2)
 {
-    bool carryOut = false;
-    deque<char>::iterator it = palindrome.end();
-    it--;
-    do
+    int increment = 0;
+    if(indexPivot1 == indexPivot2)
     {
-        if(*it == '9')
+        increment++;
+    }
+
+    for(int index1 = indexPivot1 - increment, index2 = indexPivot2 + increment; index1 >= 0; index1--, index2++)
+    {
+        palindrome[index2] = palindrome[index1];
+    }
+}
+
+void nextPalindrome(deque<char>& palindrome, char* originalNumber, int originalSize)
+{
+    long size = palindrome.size();
+    bool isEven = size % 2 == 0;
+    
+    int indexPivot1, indexPivot2;
+    if(isEven)
+    {
+        indexPivot1 = (size / 2) - 1;
+        indexPivot2 = (size / 2);
+    }
+    else
+    {
+        indexPivot1 = (size / 2);
+        indexPivot2 = indexPivot1;
+    }
+
+    makeHalfPalindrome(palindrome, indexPivot1, indexPivot2);
+
+    if(isLessThan(palindrome, originalNumber, originalSize))
+    {
+        do
+        {
+            if(indexPivot1 == indexPivot2)
             {
-                *it = '0';
-                carryOut = true;
-                if(it == palindrome.begin())
+                if(palindrome[indexPivot1] == '9')
                 {
-                    break;
+                    palindrome[indexPivot1] = '0';
+                    indexPivot1--;
+                    indexPivot2++;
                 }
-                it--;
+                else
+                {
+                    palindrome[indexPivot1] += 1;
+                }
+
             }
             else
             {
-                *it += 1;
-                carryOut = false;
+                if(palindrome[indexPivot1] == '9')
+                {
+                    palindrome[indexPivot1] = '0';
+                    palindrome[indexPivot2] = '0';
+                    indexPivot1--;
+                    indexPivot2++;
+                }
+                else
+                {
+                    palindrome[indexPivot1] += 1;
+                    palindrome[indexPivot2] += 1;
+                }
             }
-    }while(carryOut);
+        }while(isLessThan(palindrome, originalNumber, originalSize) && indexPivot1 >= 0);
 
-    if(carryOut)
-    {
-        if(*it == '0')
+        if(indexPivot1 < 0)
         {
             palindrome.push_front('1');
-        }
-        else
-        {
-            *it += 1;
+            nextPalindrome(palindrome, originalNumber, originalSize);
         }
     }
-
 }
 
 int main()
@@ -74,22 +114,23 @@ int main()
         scanf("%s", number);
         deque<char> palindrome;
         char *ptr = number;
+        int originalSize = 0;
         while(*ptr)
         {
             palindrome.push_back(*ptr);
             ptr++;
+            originalSize++;
         }
-
-        do
+        
+        nextPalindrome(palindrome, number, originalSize);
+        ptr = number;
+        for(deque<char>::iterator it = palindrome.begin(); it != palindrome.end(); it++, ptr++)
         {
-            addOne(palindrome);
-        }while(!isPalindrome(palindrome));
-
-        for(deque<char>::iterator it = palindrome.begin(); it != palindrome.end(); it++)
-        {
-            printf("%c", *it);
+            *ptr = *it;
         }
-        printf("\n");
+        *ptr = '\0';
+
+        printf("%s\n", number);
     }
 
     return 0;
